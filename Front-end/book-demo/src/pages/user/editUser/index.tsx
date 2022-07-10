7af5e103781c3de274a42307121233a2c3623c9a
ASUS <om.delvadia02@gmail.com>
2022-07-10 10:05:47 +0530

    initial commit

---
+++ Front-end/book-demo/src/pages/user/editUser/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const EditUser = () => {
+    return (
+        <>
+            <Typography variant="h1">EditUser</Typography>
+        </>
+    );
+};
+
+export default EditUser;
